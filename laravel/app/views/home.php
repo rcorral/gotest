29addb74872e409dc61623bedc76b0ee47aa51da
m1ck3y <jon.kuperman@gmail.com>
2013-05-27 19:05:20 -0400

    minor style tweaks

--- laravel/app/views/home.php
+++ laravel/app/views/home.php
@@ -3,6 +3,6 @@
 	<h1 class="home-title museo">Create Unlimited Tests. For Free.</h1>
 	<form class="intro">
 		<div class="button"><input type="text" name="name" placeholder="Enter A Test Name" /></div>
-		<input type="submit" value="Create Your Test" class="started" />
+		<input type="submit" value="Get Started!" class="started" />
 	</form>
 </section>
