f2871fc2f70db32a576e460a97f4c0abae8eadfa
Rafael Corral <rxcorral@gmail.com>
2013-05-29 20:27:13 -0400

    Added table data to migrations.

--- laravel/app/database/migrations/2013_05_27_232304_create_groups_table.php
+++ laravel/app/database/migrations/2013_05_27_232304_create_groups_table.php
@@ -12,13 +12,25 @@ class CreateGroupsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('groups', function(Blueprint $table)
-		{
-			$table->increments('id')->unsigned();
-			$table->string('name', 255)->unique();
-			$table->text('permissions')->nullable();
-			$table->timestamps();
-		});
+		$prefix = Config::get('database.connections.mysql.prefix');
+		DB::statement("CREATE TABLE `{$prefix}groups` (
+		  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+		  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+		  `permissions` text COLLATE utf8_unicode_ci,
+		  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+		  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+		  PRIMARY KEY (`id`),
+		  UNIQUE KEY `groups_name_unique` (`name`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+		");
+
+		DB::statement("INSERT INTO `{$prefix}groups` (`id`, `name`, `permissions`, `created_at`, `updated_at`)
+		VALUES
+			(1,'Admin','{\"admin\":1,\"teachers\":1,\"users\":1}','2013-05-27 03:02:51','2013-05-27 03:02:51'),
+			(2,'Teacher','{\"teachers\":1,\"student\":1}','2013-05-27 03:00:20','2013-05-27 03:00:20'),
+			(3,'User','{\"users\":1}','2013-05-27 03:04:04','2013-05-27 03:04:04');
+		");
+
 	}
 
 	/**
